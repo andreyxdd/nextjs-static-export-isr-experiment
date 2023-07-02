f563051a5c8bf615be623b6abd9dcaa9d7d64ae3
unknown <andreyxdd@yandex.ru>
2023-07-01 22:36:33 -0600

    Initial commit
    Added next-i18next package
    
    Created sample page with multiple translations

---
+++ src/pages/test.tsx
@@ -0,0 +1,24 @@
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import styles from '@/styles/Home.module.css';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
+
+export default function Test() {
+  const { t } = useTranslation('common')
+
+  return (
+    <main className={`${styles.main}`}>
+      <div>
+        <h3>Here is your translation</h3>
+        <p>{t('test-content')}</p>
+      </div>
+    </main>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  const result = await serverSideTranslations(locale as string, ['common']);
+  return {
+    props: { ...result }
+  };
+}
